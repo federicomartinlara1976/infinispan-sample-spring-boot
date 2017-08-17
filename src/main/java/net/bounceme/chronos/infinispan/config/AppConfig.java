d5075e73116213e90193c792054855730a88e3ab
federicomartinlara <federicomartinlara1976@gmail.com>
2017-08-17 21:39:22 +0200

    Infinispan dependency and cacheManager bean

--- src/main/java/net/bounceme/chronos/infinispan/config/AppConfig.java
+++ src/main/java/net/bounceme/chronos/infinispan/config/AppConfig.java
@@ -1,6 +1,8 @@
 package net.bounceme.chronos.infinispan.config;
 
 import org.apache.commons.lang3.StringUtils;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,11 @@ public class AppConfig {
 	@Value("${infinispan.owmapikey}")
 	private String apiKey;
 
+	@Bean
+	public EmbeddedCacheManager cacheManager() {
+		 return new DefaultCacheManager();
+	}
+	
 	@Bean
 	public WeatherService weatherService() {
 		return (StringUtils.isNotBlank(apiKey)) ? new OpenWeatherMapService() : new RandomWeatherService();
