f23e52daf4a17463973b5474de61f4c6d4a4c4f7
federicomartinlara <federicomartinlara1976@gmail.com>
2017-08-17 23:54:10 +0200

    Clusterización

--- src/main/java/net/bounceme/chronos/infinispan/config/AppConfig.java
+++ src/main/java/net/bounceme/chronos/infinispan/config/AppConfig.java
@@ -4,7 +4,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,15 +26,23 @@ public class AppConfig {
 	private String apiKey;
 	
 	@Bean
+	public GlobalConfigurationBuilder clusteredConfigurationBuilder() {
+		GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
+		global.transport().clusterName("WeatherApp");
+		return global;
+	}
+	
+	@Bean
 	public ConfigurationBuilder configurationBuilder() {
 		ConfigurationBuilder config = new ConfigurationBuilder();
 		config.expiration().lifespan(5, TimeUnit.SECONDS);
+		config.clustering().cacheMode(CacheMode.DIST_SYNC);
 		return config;
 	}
 
 	@Bean
-	public EmbeddedCacheManager cacheManager(ConfigurationBuilder config) {
-		 return new DefaultCacheManager(config.build());
+	public EmbeddedCacheManager cacheManager(ConfigurationBuilder config, GlobalConfigurationBuilder global) {
+		 return new DefaultCacheManager(global.build(), config.build());
 	}
 	
 	@Bean
